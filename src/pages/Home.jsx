83e1ba28ba0c6a8e80427ef773ff090bf9c4ccea
Ihor849 <Marchelo849@outlook.com>
2023-09-17 20:30:12 +0300

    1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { HomeTitle, HomeWrapper } from "./Page.styled";
+
+
+export default function Home() {
+
+
+    return (
+      <HomeWrapper  >
+        <HomeTitle  >
+         Home Page 
+        </HomeTitle>
+        <span>
+        Span
+          </span>
+      </HomeWrapper>
+    );
+  }
---
+++ src/redux/selectors.js
@@ -0,0 +1,2 @@
+export const getTheme =  state => state.theme.theme
+export const getLang =  state => state.lang.lang
