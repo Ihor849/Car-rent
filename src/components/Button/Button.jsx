60678d67776bfea340f8d0b3ec0f999ab349bfc9
Ihor849 <Marchelo849@outlook.com>
2023-09-26 12:35:33 +0300

    1

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,20 @@
-import { StyledButton, StyledFlatButton } from "./Button.styled";
+import { StyledButton, StyledFlatButton } from './Button.styled';
 
 export const Button = ({ children, ...props }) => {
-    return <StyledButton type='button' {...props}> {children}</StyledButton>;
-  };
+  console.log(props);
+  return (
+    <StyledButton type="button" {...props}>
+      {' '}
+      {children}
+    </StyledButton>
+  );
+};
 
 export const FlatButton = ({ children, ...props }) => {
-    return <StyledFlatButton type='button' {...props}> {children}</StyledFlatButton>;
-  };
+  return (
+    <StyledFlatButton type="button" {...props}>
+      {' '}
+      {children}
+    </StyledFlatButton>
+  );
+};
